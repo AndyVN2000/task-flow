87c47ae869df86eadad553612681a86af858c042
andy <202007621@post.au.dk>
2025-07-13 21:52:52 +0200

    Added enum values to TaskStatus

--- src/test/java/com/andy/task_flow/domain/enums/TaskStatus.java
+++ src/test/java/com/andy/task_flow/domain/enums/TaskStatus.java
@@ -1,5 +1,10 @@
 package com.andy.task_flow.domain.enums;
 
 public enum TaskStatus {
-    
+    TODO,            // Default status. Task has not be started yet.
+    IN_PROGRESS,     // Work on task has started.
+    BLOCKED,         // Task is currently blocked (waiting for something).
+    IN_REVIEW,       // Task is completed, but under peer or QA review.
+    DONE,            // Task has been completed and verified.
+    CANCELLED        // Task was cancelled.
 }
