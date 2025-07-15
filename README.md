c6bff0ddf2c4e8f8cfc1ae3edd05e4756e63dca0
andy <202007621@post.au.dk>
2025-07-15 17:17:49 +0200

    Created entity for Project and added a section in README listing relevant tutorials for my own learning

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # task-flow
+
+## Relevant tutorials
+
+Handling many-to-many relationships using JPA: https://www.baeldung.com/jpa-many-to-many
---
+++ src/test/java/com/andy/task_flow/domain/entities/Project.java
@@ -0,0 +1,9 @@
+package com.andy.task_flow.domain.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Project {
+    
+    
+}
