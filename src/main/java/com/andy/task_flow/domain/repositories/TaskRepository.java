09a4c7bb19573358603b9a7859f8270eedcead03
andy <202007621@post.au.dk>
2025-07-16 14:32:44 +0200

    Specified CRUD operations in TaskRepository

--- src/main/java/com/andy/task_flow/domain/repositories/TaskRepository.java
+++ src/main/java/com/andy/task_flow/domain/repositories/TaskRepository.java
@@ -1,5 +1,17 @@
 package com.andy.task_flow.domain.repositories;
 
+import com.andy.task_flow.domain.entities.Task;
+
+import java.util.UUID;
+import java.util.Optional;
+
 public interface TaskRepository {
     
+    // Accounts for both creating a new task or updating existing task (id already exists)
+    Task save(Task task);
+
+    Optional<Task> findById(UUID id);
+
+    void delete(UUID id);
+
 }
