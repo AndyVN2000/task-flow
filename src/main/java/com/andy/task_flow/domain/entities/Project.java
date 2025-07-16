a60ff78c36c93c87d647e8543a862d8525d9f081
andy <202007621@post.au.dk>
2025-07-16 13:48:09 +0200

    Added @OneToMany annotation to Project.java

--- src/main/java/com/andy/task_flow/domain/entities/Project.java
+++ src/main/java/com/andy/task_flow/domain/entities/Project.java
@@ -2,13 +2,18 @@ package com.andy.task_flow.domain.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 import java.util.UUID;
+import java.util.Set;
 
 @Entity
 public class Project {
     
     @Id
     private final UUID id = UUID.randomUUID();
+
+    @OneToMany(mappedBy = "project")
+    private Set<Task> tasks;
 
 }
