08d80996903d9f2ed6c91c2ea1978dddef6f6e1d
andy <202007621@post.au.dk>
2025-08-04 13:40:07 +0200

    Defined domain operations for Project interface

--- src/main/java/com/andy/task_flow/domain/entities/interfaces/Project.java
+++ src/main/java/com/andy/task_flow/domain/entities/interfaces/Project.java
@@ -1,7 +1,38 @@
 package com.andy.task_flow.domain.entities.interfaces;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.andy.task_flow.domain.entities.Task;
+
 public interface Project {
     
-    
+    UUID getId();
+
+    String getName();
+
+    String getDescription();
+
+    Instant getCreatedAt();
+
+    Optional<Instant> getArchivedAt();
+
+    Optional<String> getArchivedBy();
+
+    /**
+     * External mutation on the list of tasks should not be possible.
+     * @return an immutable list of tasks.
+     */
+    List<Task> getTasks();
+
+    boolean isArchived();
+
+    int getCompletedTaskCount();
+
+    // TODO: this method is weird. Must think about how to deal with it
+    boolean hasOverdueTasks(Clock clock);
 
 }
