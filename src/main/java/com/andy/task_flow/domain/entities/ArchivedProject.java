f568e2d2a15b1384a20073c03fa7e218699cc7cb
andy <202007621@post.au.dk>
2025-08-25 15:22:23 +0200

    Implemented fake-it code in ArchivedProject.java. To be tested

--- src/main/java/com/andy/task_flow/domain/entities/ArchivedProject.java
+++ src/main/java/com/andy/task_flow/domain/entities/ArchivedProject.java
@@ -1,7 +1,55 @@
 package com.andy.task_flow.domain.entities;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import com.andy.task_flow.domain.entities.interfaces.Project;
 
 public class ArchivedProject implements Project {
+
+
+
+    public UUID getId() {
+        return null;
+    }
+
+    public String getName() {
+        return "";
+    }
+
+    public String getDescription() {
+        return "";
+    }
+
+    public Instant getCreatedAt() {
+        return null;
+    }
+
+    public Optional<Instant> getArchivedAt() {
+        return null;
+    }
+
+    public Optional<String> getArchivedBy() {
+        return null;
+    }
+
+    public List<Task> getTasks() {
+        return null;
+    }
+
+    public boolean isArchived() {
+        return true;
+    }
+
+    public int getCompletedTaskCount() {
+        return 0;
+    }
+
+    public boolean hasOverdueTasks(Clock clock) {
+        return false;
+    }
     
 }
