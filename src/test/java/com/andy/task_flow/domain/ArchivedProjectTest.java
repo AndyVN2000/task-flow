18962364337015610e0960c16a6201916b98b494
andy <202007621@post.au.dk>
2025-09-10 09:51:34 +0200

    Added test case for ArchivedProject. Problem with instantiating projects with specific tasks remains

--- src/test/java/com/andy/task_flow/domain/ArchivedProjectTest.java
+++ src/test/java/com/andy/task_flow/domain/ArchivedProjectTest.java
@@ -1,8 +1,15 @@
 package com.andy.task_flow.domain;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Clock;
 import java.time.Instant;
+import java.time.ZoneId;
+
+import org.junit.jupiter.api.Test;
 
 import com.andy.task_flow.domain.entities.ArchivedProject;
+import com.andy.task_flow.domain.entities.Task;
 import com.andy.task_flow.domain.entities.interfaces.Project;
 
 public class ArchivedProjectTest extends ProjectContractTest {
@@ -10,6 +17,20 @@ public class ArchivedProjectTest extends ProjectContractTest {
     @Override
     protected Project createProject() {
         return ArchivedProject.of("Foo", "Bar", Instant.MIN, "John Doe");
+    }
+
+    @Test
+    public void archivedProjectStillReportsOverdueTasks() {
+        // Instantiate project with an overdue task
+        ArchivedProject project;
+
+        Task task = project.getTasks().get(0);
+        Instant dueDate = task.getDueDate().get();
+        Instant currentDate = Instant.EPOCH;
+
+        assertTrue(dueDate.isBefore(currentDate));
+
+        assertTrue(project.hasOverdueTasks(Clock.fixed(currentDate, ZoneId.of("Europe/Paris"))));
     }
     
 }
