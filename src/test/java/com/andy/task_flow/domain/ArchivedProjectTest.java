1b8c9ebb8c3ca81c5c858ff972c760e34c521c74
andy <202007621@post.au.dk>
2025-09-16 15:32:34 +0200

    Test case: ArchivedProject still reports overdue tasks

--- src/test/java/com/andy/task_flow/domain/ArchivedProjectTest.java
+++ src/test/java/com/andy/task_flow/domain/ArchivedProjectTest.java
@@ -5,16 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.andy.task_flow.domain.entities.ArchivedProject;
-import com.andy.task_flow.domain.entities.TaskImpl;
 import com.andy.task_flow.domain.entities.interfaces.Project;
 import com.andy.task_flow.domain.entities.interfaces.ProjectBuilder;
+import com.andy.task_flow.domain.entities.interfaces.Task;
+import com.andy.task_flow.fixtures.builders.TaskBuilder;
 
 public class ArchivedProjectTest extends ProjectContractTest {
+    private Instant currentDate;
 
     @Override
     protected Project createProject() {
@@ -26,18 +31,26 @@ public class ArchivedProjectTest extends ProjectContractTest {
         return new ArchivedProject.ArchivedProjectBuilder();
     }
 
-    // @Test
-    // public void archivedProjectStillReportsOverdueTasks() {
-    //     // Instantiate project with an overdue task
-    //     ArchivedProject project;
+    @BeforeEach
+    private void setUp() {
+        currentDate = Instant.EPOCH;
+    }
 
-    //     Task task = project.getTasks().get(0);
-    //     Instant dueDate = task.getDueDate().get();
-    //     Instant currentDate = Instant.EPOCH;
+    @Test
+    public void archivedProjectStillReportsOverdueTasks() {
+        // Instantiate project with an overdue task
+        TaskBuilder taskBuilder = createTaskBuilder();
+        Task overdueTask = taskBuilder.setDueDate(Optional.of(currentDate.minus(1, ChronoUnit.DAYS)))
+            .build();
+        ProjectBuilder projectBuilder = createProjectBuilder();
+        ArchivedProject project = (ArchivedProject) projectBuilder.addTask(overdueTask).build();
 
-    //     assertTrue(dueDate.isBefore(currentDate));
+        Task task = project.getTasks().get(0);
+        Instant dueDate = task.getDueDate().get();
 
-    //     assertTrue(project.hasOverdueTasks(Clock.fixed(currentDate, ZoneId.of("Europe/Paris"))));
-    // }
+        assertTrue(dueDate.isBefore(currentDate));
+
+        assertTrue(project.hasOverdueTasks(Clock.fixed(currentDate, ZoneId.of("Europe/Paris"))));
+    }
     
 }
