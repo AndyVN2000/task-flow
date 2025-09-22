3a98a7a18c3b88fa384f85f9fe0dc97eb360592f
andy <202007621@post.au.dk>
2025-09-22 15:49:03 +0200

    Did some ugly type casting. A possible alternative is to move all mutator methods in MutableProject to Project

--- src/main/java/com/andy/task_flow/application/services/ProjectApplicationService.java
+++ src/main/java/com/andy/task_flow/application/services/ProjectApplicationService.java
@@ -1,6 +1,10 @@
 package com.andy.task_flow.application.services;
 
+import java.time.Clock;
+import java.util.UUID;
+
 import com.andy.task_flow.domain.entities.ProjectImpl;
+import com.andy.task_flow.domain.entities.interfaces.MutableProject;
 import com.andy.task_flow.domain.entities.interfaces.Project;
 import com.andy.task_flow.domain.repositories.ProjectRepository;
 
@@ -14,6 +18,11 @@ public class ProjectApplicationService {
     public void createProject(String title, String description) {
         Project project = ProjectImpl.of(title, description);
         projectRepository.save(project);
+    }
+
+    public void archiveProject(UUID projectId, String archivedBy, Clock clock) {
+        MutableProject project = (MutableProject) projectRepository.findById(projectId).get();
+        project.archive(archivedBy, clock);
     }
 
 }
