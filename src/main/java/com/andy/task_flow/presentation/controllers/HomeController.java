e545cb1da39c50da1ad129599c893ab8aaa2ce24
andy <202007621@post.au.dk>
2025-11-22 18:13:44 +0100

    Added presentation folder and an index.html

---
+++ src/main/java/com/andy/task_flow/presentation/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.andy.task_flow.presentation.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+
+}
