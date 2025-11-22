69b73a99bd27ca5493caf3255a321ab96d765563
andy <202007621@post.au.dk>
2025-11-22 18:19:18 +0100

    Gave the label string the @Id jpa decorator

--- src/main/java/com/andy/task_flow/domain/entities/Label.java
+++ src/main/java/com/andy/task_flow/domain/entities/Label.java
@@ -4,6 +4,7 @@ import com.andy.task_flow.domain.entities.interfaces.Task;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 
 import java.util.Set;
@@ -11,6 +12,7 @@ import java.util.Set;
 @Entity
 public class Label {
 
+    @Id
     @Column
     private String label;
 
