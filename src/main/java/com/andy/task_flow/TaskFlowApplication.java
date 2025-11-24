18ac8c61923794be9957eafa4e4baa58d9b7d94b
andy <202007621@post.au.dk>
2025-11-24 22:26:43 +0100

    Trying to experiment with the 'demo' method in the SpringBootApplication class. Getting errors due to my misuse of naming conventions when using JPA query methods. Specifically, I must edit in 'ProjectRepository' interface

--- src/main/java/com/andy/task_flow/TaskFlowApplication.java
+++ src/main/java/com/andy/task_flow/TaskFlowApplication.java
@@ -1,13 +1,34 @@
 package com.andy.task_flow;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.andy.task_flow.domain.entities.ProjectImpl;
+import com.andy.task_flow.domain.entities.interfaces.Project;
+import com.andy.task_flow.domain.repositories.ProjectRepository;
 
 @SpringBootApplication
 public class TaskFlowApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(TaskFlowApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(TaskFlowApplication.class, args);
+	}
+
+	/**
+	 * The following method is inspired by: https://spring.io/guides/gs/accessing-data-jpa
+	 */
+	@Bean
+	public CommandLineRunner demo(ProjectRepository repository) {
+		return (args) -> {
+			logger.info("Creating projects");
+			repository.save(ProjectImpl.of("Foo", "Bar"));
+		};
 	}
 
 }
